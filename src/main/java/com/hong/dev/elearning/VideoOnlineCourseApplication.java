9a63b272015e04801ac3113786aa5d6817b74615
seyhongkin <kin19seyhong@gmail.com>
2023-05-01 16:55:08 +0700

    initialize project

---
+++ src/main/java/com/hong/dev/elearning/VideoOnlineCourseApplication.java
@@ -0,0 +1,13 @@
+package com.hong.dev.elearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoOnlineCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoOnlineCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hong/dev/elearning/VideoOnlineCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.dev.elearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoOnlineCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
